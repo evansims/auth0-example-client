39c05c0d63fe6116869f6ee480f40f1f6172efe5
Evan Sims <hello@evansims.com>
2020-09-14 00:35:11 -0400

    Remove obsolete services

--- app/services/site.js
+++ app/services/site.js
@@ -15,36 +15,6 @@ export default Service.extend({
   appLoadingState: null,
   appLoadingComplete: false,
 
-  getMeta: function(reload) {
-    reload = reload || false;
-
-    if (!reload && get(this, 'meta')) {
-      return new RSVP.Promise(resolve => {
-        return resolve(get(this, 'meta'));
-      });
-    }
-
-    if (window.auth0 && window.auth0.metadata) {
-      return new RSVP.Promise(resolve => {
-        set(this, 'meta', window.auth0.metadata);
-        return resolve(window.auth0.metadata);
-      });
-    }
-
-    return new RSVP.Promise((resolve, reject) => {
-      get(this, 'ajax')
-        .request(ENV.APP.apiHost + '/v1/meta', { method: 'GET' })
-        .then(response => {
-          set(this, 'meta', response.meta);
-
-          return resolve(get(this, 'meta'));
-        })
-        .catch(error => {
-          return reject(error);
-        });
-    });
-  },
-
   isPrivateBrowsing: computed('navigator.doNotTrack', function() {
     return !!(navigator.doNotTrack || 0);
   }),
